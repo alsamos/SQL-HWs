e96c48159e5d69a3d27d45df8e1263745cd4c613
alsamos <108459502+alsamos@users.noreply.github.com>
2023-04-17 22:56:04 +0300

    Add files via upload

---
+++ hw_sql_2.sql
@@ -0,0 +1,68 @@
+-- Урок 2. SQL – создание объектов, простые запросы выборки
+-- 1. Используя операторы языка SQL, создайте табличку “sales”. Заполните ее данными
+
+DROP DATABASE IF EXISTS sales; 
+CREATE DATABASE sales;
+USE sales;
+
+DROP TABLE IF EXISTS sales; 
+CREATE TABLE sales
+(
+	id INT PRIMARY KEY AUTO_INCREMENT, 
+    order_date DATE,
+    count_product INT
+);
+
+INSERT INTO sales(order_date, count_product)
+VALUES 
+	("2022-01-01", 156), 
+    ("2022-01-02", 180), 
+    ("2022-01-03", 21), 
+    ("2022-01-04", 124), 
+    ("2022-01-05", 341);
+
+SELECT * FROM sales;
+
+-- 2. Сгруппируйте значений количества в 3 сегмента — меньше 100, 100-300 и больше 300.
+
+SELECT 
+	id,
+    IF (count_product < 100, "Маленький заказ",
+		IF(count_product BETWEEN 100 AND 300, "Средний заказ", "Большой заказ"))
+	AS "Тип заказа"
+FROM sales;
+
+-- 3. Создайте таблицу “orders”, заполните ее значениями. Покажите “полный” статус заказа, используя оператор CASE.
+
+DROP TABLE IF EXISTS orders;
+CREATE TABLE orders
+(
+	id INT PRIMARY KEY AUTO_INCREMENT, 
+    employee_id VARCHAR(3) NOT NULL,
+    amount DECIMAL(5.2),
+    order_status VARCHAR(9)
+);
+
+SELECT * FROM orders;
+
+INSERT INTO orders (employee_id, amount, order_status)
+VALUES 
+	("e03", 15.00, "OPEN"), 
+    ("e01", 25.50, "OPEN"), 
+    ("e05", 100.70, "CLOSED"), 
+    ("e02", 22.18, "OPEN"), 
+    ("e04", 9.50, "CANCELLED");
+
+SELECT * FROM orders;
+
+SELECT 
+	id,
+    employee_id,
+    amount, 
+CASE
+	WHEN order_status = "OPEN" THEN "Order is in open state"
+    WHEN order_status = "CLOSED" THEN "Order is closed"
+    WHEN order_status = "CANCELLED" THEN "Order is cancelled"
+END AS "full_order_status"
+FROM orders;
+
